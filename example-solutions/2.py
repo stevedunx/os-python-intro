b246b6267b25b691e8ed5848e621480fa9d5dc61
Sarah Beharry-Goss <sarahbeharrygoss@users.noreply.github.com>
2017-02-20 14:22:38 +0000

    Create 2.py

---
+++ example-solutions/2.py
@@ -0,0 +1,20 @@
+# Part 1
+
+nameVariable = "Miss Elaine E. Yuss"
+print("Hello {name}".format(name=nameVariable))
+
+# Or you could do...
+#print("Hello {0}".format(nameVariable))
+#print("Hello " + nameVariable)
+
+# Part 2
+
+print('"The quote marks are mandatory"')
+
+print("A quote from Poe: 'Quoth the raven: nevermore'")
+
+print('''Haikus are easy.
+But sometimes they don’t make sense. 
+Refrigerator''')
+
+print(r"Dashes\are\a\novel\way\of\separating\words")
