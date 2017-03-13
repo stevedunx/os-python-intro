5fdbb9ef3bcbf23d0f654545b066d5fa058ec142
Steve Duncan <steve.duncan@os.uk>
2017-03-13 19:54:44 +0000

    simplified script (probably)

--- example-solutions/5-boolean.py
+++ example-solutions/5-boolean.py
@@ -7,12 +7,10 @@ maximum_delivery_distance = 250
 buyers_distance_from_shop = 208
 
 # restock required
-if order_quantity == item_stock:
-    print "restock required"
+if order_quantity == item_stock: print "restock required"
 
 # enough in stock
-if order_quantity <= item_stock:
-    print "enough"
+if order_quantity <= item_stock: print "enough"
 
 # buyer wants delivery and is within delivery distance
 if buyer_wants_delivery and buyers_distance_from_shop <= maximum_delivery_distance:
