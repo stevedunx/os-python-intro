3fcc77675e56513abb1b0e8c3476ed46c32f2515
Steve Duncan <stevedunx@users.noreply.github.com>
2017-03-17 16:00:28 +0000

    Update 8-bonus-modules.py

--- example-solutions/8-bonus-modules.py
+++ example-solutions/8-bonus-modules.py
@@ -2,7 +2,7 @@ import os
 import glob, csv, time
 import re
 
-example_data_dir = os.path.join('..','exercises','exercise-8-data','*') # TODO: limit to just CSV files
+example_data_dir = os.path.join('..','exercises','exercise-8-data','*.*') # TODO: limit to just CSV files
 
 # Loop through all of the files that match the path pattern
 for csv_path in glob.glob(example_data_dir):
